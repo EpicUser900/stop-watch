659ab73705dc80e9d3e0c1673408060802e8c266
EpicUser900 <79115827+EpicUser900@users.noreply.github.com>
2021-02-18 11:56:23 +1300

    ()[]{}<>

--- main.ts
+++ main.ts
@@ -5,5 +5,5 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     elapsed = input.runningTime() - start
-    basic.showString("" + elapsed / 1000 + "ms")
+    basic.showNumber(elapsed / 1000)
 })
